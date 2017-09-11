7c512e09ba517350c002aa58cfd18cdf3ef5ee21
Niclas Hedam <niclas@frax.dk>
2017-09-11 13:12:49 +0200

    Now delcaring the attribute correctly

--- src/NiclasHedam/EloquentRound/RoundsDecimals.php
+++ src/NiclasHedam/EloquentRound/RoundsDecimals.php
@@ -3,12 +3,13 @@ namespace NiclasHedam\EloquentRound;
 
 trait RoundsDecimals
 {
+    public $rounds = [];
+
     public function setAttribute($key, $value)
     {
-        if (!isset($this->rounds) || !is_array($this->rounds)) {
+        if (!isset($this->rounds) || !is_array($this->rounds) || !isset($this->rounds[$key])) {
             return parent::setAttribute($key, $value);
         }
-
 
         if (isset($this->rounds[$key]) && !is_int($this->rounds[$key])) {
             throw new \Exception('Rounds has to be mapped $key => int');
